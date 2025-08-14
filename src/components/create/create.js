86367340233e45ec2ea57da5de5ef8985eab7ec2
shreyansh225 <225shreyansh@gmail.com>
2025-08-14 23:18:35 +0530

    Restore tracked files, ignore cache and dependencies

---
+++ src/components/create/create.js
@@ -0,0 +1,83 @@
+import React, {useState, useEffect}  from 'react'
+// import {Form, FormGroup, Button, Label, Input } from 'reactstrap'
+import axios from 'axios';
+import { Link,useNavigate  } from 'react-router-dom';
+
+function Create() {
+    const navigate = useNavigate();
+    // let history = useHistory();
+    const[firstName, setFirstName] = useState('');
+    const[lastName, setLastName] = useState('');
+
+    const sendDataToAPI = () => {
+        // Validation check
+        if (!firstName.trim() || !lastName.trim()) {
+            alert("❌ Please fill out both First Name and Last Name.");
+            return;
+        }
+        
+        // Sending data to API
+        axios.post(`https://627fa751b1cc1b12625886d2.mockapi.io/crud` , {
+            firstName,
+            lastName
+        })
+        .then(() => {
+            setFirstName('');
+            setLastName('');
+            alert("Data Added Successfully");
+            navigate('/read');  // Redirect to read page
+            // history.push('./read');
+        }
+            // res => console.log('posting data', res)
+            )
+        .catch(err => console.log(err))
+
+        console.warn(firstName, lastName);
+        console.log(firstName);
+        console.log(lastName);
+        
+    }
+    
+  return (
+    
+        <div className='contentBox'>
+            <div className="formBox">
+                <h2> React CRUD Operations </h2>
+                    <form>
+                        <div className="inputBox">
+                            <span> First Name </span>
+                            <input  
+                            name="fname" 
+                            value={firstName}
+                            onChange= {(e) => setFirstName(e.target.value)}  
+                            placeholder='Enter first name' />
+                        </div>
+                        <div className="inputBox">
+                            <span> Last Name </span>
+                            <input 
+                            name="lname"  
+                            value={lastName}
+                            onChange= {(e) => setLastName(e.target.value)}  
+                            placeholder='Enter last name'/>
+                        </div>
+                        {/* <div className="remember">
+                            <label><input type="checkbox" name="" id="" /> Remember me</label>
+                        </div> */}
+                        <div className="inputBox">
+                            {/* <Link to ='/read'> */}
+                                <input type="button" value="Submit" onClick={sendDataToAPI}  />
+                            {/* </Link> */}
+                        </div>
+                        <div className="inputBox">
+                            <Link to ='/read'>
+                                <input type="button" value="Show Data"  />
+                            </Link>
+                        </div>
+                    </form>
+            </div>
+        </div>
+    
+  )
+}
+
+export default Create;
---
+++ src/components/read/read.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+// import {Table } from 'reactstrap'
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function Read() {
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://627fa751b1cc1b12625886d2.mockapi.io/crud`)
+      .then((getData) => {
+        setApiData(getData.data);
+      });
+  }, []);
+
+  const setData = (id, firstName, lastName) => {
+    console.log(id);
+    localStorage.setItem("ID", id);
+    localStorage.getItem("firstName");
+    localStorage.getItem("lastName");
+  };
+
+  const getData = () => {
+    axios
+      .get(`https://627fa751b1cc1b12625886d2.mockapi.io/crud`)
+      .then((getData) => {
+        setApiData(getData.data);
+      });
+  };
+
+  const onDelete = (id) => {
+    axios
+      .delete(`https://627fa751b1cc1b12625886d2.mockapi.io/crud/${id}`)
+      .then(() => {
+        getData();
+      });
+  };
+
+  return (
+    <>
+      <div className="tableBox">
+        <table>
+          <thead className="thead-class">
+            <tr>
+              <th>Number</th>
+              <th>F Name </th>
+              <th>L Name </th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {apiData.map((data,index) => {
+              return (
+                <tr key={data.id}>
+                  <th scope="row">{data.id}</th>
+                  <td>{data.firstName}</td>
+                  <td>{data.lastName}</td>
+                  <td>
+                    <Link to="/update">
+                      {/* onClick={() => setID(data.id)} */}
+                      <button
+                        type="button"
+                        value="firstName"
+                        onClick={() =>
+                          setData(data.id, data.firstName, data.lastName)
+                        }
+                        className="update"
+                      >
+                        Update
+                      </button>
+                    </Link>
+                  </td>
+                  <td>
+                    <Link to="/read">
+                      <button
+                        type="button"
+                        value="lastName"
+                        onClick={() => onDelete(data.id)}
+                        className="delete"
+                      >
+                        Delete
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      
+      <div className="contentBox">
+        <div className="formBox">
+          <form className="inputBox">
+            <Link to="/create">
+              <input type="button" value="Add New Data" />
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Read;
